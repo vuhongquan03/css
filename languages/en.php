21b202c67de9a7f7075d384198897f2925211fbb
Vu Hong Quan <36039431+vuhongquan03@users.noreply.github.com>
2019-03-25 17:06:56 +0700

    Add files via upload

---
+++ languages/en.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Twitter&reg; Bootstrap 4.1 Theme for Elgg
+ *
+ * Converts all Elgg css elements to Twitter&reg; Bootstrap elements.  Helps 
+ * Designers create beautiful Bootstrap themes for Elgg.
+ *
+ * @category   Elgg Themes
+ * @author     Shane Barron <admin@socia.us>
+ * @copyright  2018 SocialApparatus / ElggDesign
+ * @version    1.0
+ * @link       http://socia.us
+ */
+return array(
+    "my_account" => "My Account",
+    "my_profile" => "My Profile",
+    "my_messages" => "<i class='fa fa-envelope'></i> (%s)",
+    "site_notifications:topbar"=>"<i class='fa fa-bullhorn'></i>",
+    "panel-primary" => "Panel Primary",
+    "panel-default" => "Panel Default",
+    "panel-success" => "Panel Success",
+    "panel-info" => "Panel Info",
+    "panel-warning" => "Panel Warning",
+    "panel-danger" => "Panel Danger",
+    "default" => "Default",
+    "cerulean" => "Cerulean",
+    "cosmo" => "Cosmo",
+    "cyborg" => "Cyborg",
+    "darkly" => "Darkly",
+    "flatly" => "Flatly",
+    "journal" => "Journal",
+    "lumen" => "Lumen",
+    "paper" => "Paper",
+    "readable" => "Readable",
+    "sandstone" => "Standstone",
+    "simplex" => "Simplex",
+    "slate" => "Slate",
+    "solar" => "Solar",
+    "spacelab" => "Spacelab",
+    "superhero" => "Superhero",
+    "united" => "United",
+    "yeti" => "Yeti"
+);
